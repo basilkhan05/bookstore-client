8c11b1fd6e49ee657e3630878bb8922407c1e645
Basil Khan <basilkhan05@gmail.com>
2019-01-08 08:14:33 -0500

    fix(Adapter&Model): Application and author respectively, based on console err

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import { underscore } from '@ember/string';
 
-const { String: { pluralize, underscore } } = Ember;
+const { String: { pluralize } } = Ember;
 
 export default DS.JSONAPIAdapter.extend({
 
